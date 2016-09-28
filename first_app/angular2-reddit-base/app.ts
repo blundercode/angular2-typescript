ad23021edf78ca72f124c65c3021de3530965387
Erik McFarland <ErikMcFarland@chemdry.com>
2016-09-28 17:02:02 -0600

    added form and functionality

--- first_app/angular2-reddit-base/app.ts
+++ first_app/angular2-reddit-base/app.ts
@@ -7,25 +7,34 @@ import {
   Component
 } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-Browser-Dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 
 @Component({
   selector: 'reddit',
   template: `
-    <form class=" ui large form segment">
-      <h3 class="ui header">Add a Link</h3>
-      <div class="field">
-        <label for="title">Title:</label>
-        <input name="title">
-      </div>
-      <div class="field">
-        <label for="link">Link:</label>
-        <input name="link">
-      </div> 
-    </form>
-  `
-}) class RedditApp { }
+ <form class="ui large form segment">
+   <h3 class="ui header">Add a Link</h3>
+   <div class="field">
+     <label for="title">Title:</label>
+     <input name="title" #newtitle> 
+   </div>
+   <div class="field">
+     <label for="link">Link:</label>
+     <input name="link" #newlink> 
+   </div>
+
+ <!-- added this button -->
+   <button (click)="addArticle(newtitle, newlink)" class="ui positive right floated button">Submit link</button>
+ </form>
+`
+})
+class RedditApp {
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
+}
 
 @NgModule({
   declarations: [
