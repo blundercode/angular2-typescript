92051b2cb5fd5d324e3886918190fb7ab8d55260
Erik <www.erik.mcland@gmail.com>
2016-12-14 16:06:17 -0700

    tset

--- first_app/angular2-reddit-base/app.ts
+++ first_app/angular2-reddit-base/app.ts
@@ -9,11 +9,6 @@ import {
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-/**
- * 
- * 
- * @class Article
- */
 @Component({
   selector: 'reddit-article',
   host: {
@@ -22,12 +17,16 @@ import {platformBrowserDynamic } from '@angular/platform-browser-dynamic';
   template: `
     <div class="four wide column center aligned votes">
       <div class="ui statistic">
-        <div class="value">{{ votes }}</div>
+        <div class="value">
+          {{ article.votes }}
+        </div>
         <div class="label">Points</div>
       </div>
     </div>
     <div class="twelve wide column">
-      <a class="ui large header" href="{{ link }}">{{ title }}</a>
+      <a class="ui large header" href="{{ article.link }}">
+        {{ article.title }}
+      </a>
       <ul class="ui big horizontal list voters">
         <li class="item">
           <a href (click)="voteUp()"><i class="arrow up icon"></i>upvote</a>
@@ -42,40 +41,27 @@ import {platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 class Article {
-  /**
-   * 
-   * 
-   * @type {string}
-   * @memberOf Article
-   */
   title: string;
-  /**
-   * 
-   * 
-   * @type {string}
-   * @memberOf Article
-   */
   link: string;
-  /**
-   * 
-   * 
-   * @type {number}
-   * @memberOf Article
-   */
   votes: number;
-  /**
-   * Creates an instance of Article.
-   * 
-   * @param {string} title
-   * @param {string} link
-   * @param {number} [votes]
-   * 
-   * @memberOf Article
-   */
   constructor(title: string, link: string, votes?: number) {
     this.title = title;
     this.link = link;
     this.votes = votes || 0
+  }
+  voteUp(): void {
+    this.votes += 1;
+  }
+  voteDown(): void {
+    this.votes -= 1
+  }
+  domain(): string {
+    try {
+      const link: string = this.link.split('//')[1];
+      return link.split('1')[0];
+    } catch (err) {
+      return null;
+    }
   }
 }
 
@@ -84,73 +70,19 @@ class Article {
  * 
  * @class ArticleComponent
  */
-/**
- * 
- * 
- * @class ArticleComponent
- */
 class ArticleComponent {
-  /**
-   * 
-   * 
-   * @type {number}
-   * @memberOf ArticleComponent
-   */
-  votes: number;
-  /**
-   * 
-   * 
-   * @type {string}
-   * @memberOf ArticleComponent
-   */
-  title: string;
-  /**
-   * 
-   * 
-   * @type {string}
-   * @memberOf ArticleComponent
-   */
-  link: string;
-  /**
-   * Creates an instance of ArticleComponent.
-   * 
-   * 
-   * @memberOf ArticleComponent
-   */
-  constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
-  }
-  /**
-   * 
-   * 
-   * @returns {boolean}
-   * 
-   * @memberOf ArticleComponent
-   */
-  voteUp(): boolean {
-    this.votes += 1;
-    return false
-  }
-  /**
-   * 
-   * 
-   * @returns {boolean}
-   * 
-   * @memberOf ArticleComponent
-   */
-  voteDown(): boolean {
-    this.votes -= 1;
-    return false
-  }
-} 
+  article: Article;
 
-/**
- * 
- * 
- * @class RedditApp
- */
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false
+  }
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false
+  }
+}
+
 /**
  * 
  * 
@@ -181,36 +113,22 @@ class ArticleComponent {
 })
 
 class RedditApp {
-  /**
-   * 
-   * 
-   * @param {HTMLInputElement} title
-   * @param {HTMLInputElement} link
-   * @returns {boolean}
-   * 
-   * @memberOf RedditApp
-   */
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    var a = 1;
-    console.log(a + 1);
-    console.log(a += 1);
-    return false;
+  articles: Article[];
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
   }
-}
 
-/**
- * 
- * 
- * @class RedditAppModule
- */
-@NgModule({
-  declarations: [
-    RedditApp,
-    ArticleComponent 
-  ],
-  imports: [BrowserModule],
-  bootstrap: [RedditApp]
-}) class RedditAppModule { }
+  @NgModule({
+    declarations: [
+      RedditApp,
+      ArticleComponent
+    ],
+    imports: [BrowserModule],
+    bootstrap: [RedditApp]
+  }) class RedditAppModule {}
 
 platformBrowserDynamic().bootstrapModule(RedditAppModule);
